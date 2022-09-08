5d580c30535c03483fcdc55c49487e523ba43050
codewithsahi <codewwithsahi@gmail.com>
2022-09-08 16:22:54 -0700

    ohgyg

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,9 +4,9 @@
  *  Description: using the main function
  *  this program prints "Programming is like building a multilingual puzzle
  *  Return: always (0)
- * */
+ **/
 int main(void)
 {
 	 puts("\"Programming is like building a multilingual puzzle");
-	  return (0);
+	 return (0);
 }
