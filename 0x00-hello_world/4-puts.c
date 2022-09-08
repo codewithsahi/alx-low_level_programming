39c19ac0e30b0b6015b9690987513ef0c4977715
codewithsahi <codewwithsahi@gmail.com>
2022-09-08 16:17:36 -0700

    4-putss

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,23 +1,12 @@
 #include <stdio.h>
-
 /**
- *
- * * main - print the string in the put function
- *
- * *
- *
- * * Description: using the main function
- *
- * * this program prints "Programming is like building a multilingual puzzle
- *
- * * Return: 0
- *
+ *  main - print the string in the put function
+ *  Description: using the main function
+ *  this program prints "Programming is like building a multilingual puzzle
+ *  Return: always (0)
  * */
-
 int main(void)
-
 {
-	 printf("with proper grammar, but the outcome is a piece of art,\n");
-
+	 puts("\"Programming is like building a multilingual puzzle");
 	  return (0);
 }
