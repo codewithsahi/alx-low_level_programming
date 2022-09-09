795cfd19bb75996dfe6939351c1bb3392f9d8399
codewithsahi <codewwithsahi@gmail.com>
2022-09-08 23:02:17 -0700

    printing with betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - entry point
+ * main - print the string in the putfunction
  *
  *Return: always (0)
  */
 int main(void)
 {
-	 puts("\"Programming is like building a multilingual puzzle");
-		 return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
