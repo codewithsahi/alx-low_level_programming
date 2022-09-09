a5ce749c00e95256b268b1bfa4b90e71a5699bbf
codewithsahi <codewwithsahi@gmail.com>
2022-09-09 02:11:06 -0700

    correctionsss

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-
-
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+/**
+ * main -print the string in the printf function
+ * Return: always (0)
+ */
+int main(void)
+{
+	printf("with proper grammar, but theoutcome is a piece of art,\n");
 	return (0);
 }
