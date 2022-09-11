4a4bc0b0986751e6ad01654164e94b5515541016
codewithsahi <codewwithsahi@gmail.com>
2022-09-11 03:13:35 -0700

    quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,8 @@
 
 int main(void)
 {
-	write(1, "and that piece of art is useful\" Dora Korpar, ", 59);
-	write(1, "2015-10-19", 59);
+	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	
+	write(2, str1, 59);
 	return (1);
 }
