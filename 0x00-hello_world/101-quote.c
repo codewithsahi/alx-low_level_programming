38a03cfa5169c8eb761b0d35386be70e7636e11c
codewithsahi <codewwithsahi@gmail.com>
2022-09-11 02:50:20 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,15 +2,13 @@
 #include <unistd.h>
 
 /**
- *
- *  * main - prints and that piece of art is useful" - Dora Korpar, 2015-10-19
- *
- *   * Return:1
- *
- *    */
+ * main - prints and that piece of art is useful" - Dora Korpar, 2015-10-19
+ *Return: (1)
+ */
 
 int main(void)
 {
-	 write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	  return (1);
+	write(1, "and that piece of art is useful\"", 59);
+	write(1, "  - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
